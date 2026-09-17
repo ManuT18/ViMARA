44d7796b3d54dc0eebce8e4d0435d9f326f5fb86
ManuT18 <manuturo2008@gmail.com>
2026-09-16 23:47:12 -0300

    build: actualizar binarios WebGL de Unity con reticula de escaneo, world lock y optimizaciones tactiles

--- public/unity_ar/Build/unity_ar.loader.js
+++ public/unity_ar/Build/unity_ar.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 65766300,
+    wasmFileSize: 65768109,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
