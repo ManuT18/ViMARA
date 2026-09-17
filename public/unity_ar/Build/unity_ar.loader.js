9d95800a26d6aa0d8f4cee891cfc5b368ea5c0e8
ManuT18 <manuturo2008@gmail.com>
2026-09-17 00:06:21 -0300

    build: binarios comprimidos en Gzip

--- public/unity_ar/Build/unity_ar.loader.js
+++ public/unity_ar/Build/unity_ar.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 65768109,
+    wasmFileSize: 65768101,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
